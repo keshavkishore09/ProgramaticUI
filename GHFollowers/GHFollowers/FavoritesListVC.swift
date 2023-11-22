e93fb13f98032930d390bbf00515c8319a1c79aa
Keshav Kishore <Keshav.kishore@ncontr.com>
2023-11-22 14:01:51 +0900

    Add tab bar controller and it's sub container controllers

---
+++ GHFollowers/GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by FDIT MAC on 2023/11/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
