e81b11402bd2acecd74f1024d1dff82771ebf592
Keshav Kishore <Keshav.kishore@ncontr.com>
2023-11-24 13:45:25 +0900

    Custom Views (UIButton & UITextField)

---
+++ GHFollowers/GHFollowers/GFButton.swift
@@ -0,0 +1,38 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by FDIT MAC on 2023/11/24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        // Custom Code here
+        configure()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
