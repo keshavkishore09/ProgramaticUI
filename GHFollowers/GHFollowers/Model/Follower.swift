0dbd30374d20333e384b37b9dab73dbd081e118b
Keshav Kishore <keshavkishore09@gmail.com>
2023-12-01 11:22:21 +0900

    Create Models

---
+++ GHFollowers/GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GHFollowers
+//
+//  Created by Keshav Kishore on 01/12/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+    
+}
